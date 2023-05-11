9ec18e2f2ea447c29607215a2e8b0b582e9fafc4
nickash2 <nickash015@gmail.com>
2023-05-11 12:57:20 +0200

    Added Alpha-Beta Pruning

--- nim.py
+++ nim.py
@@ -1,5 +1,5 @@
 import sys
-
+import math
 
 def max_value(state):
     max = -100000000000
@@ -31,7 +31,6 @@ def min_value(state):
 
 def minimax_decision(state, turn):
     best_move = None
-    utility = 0
     if turn == 0:  # MAX' turn
         max = -100000000000
 
@@ -41,7 +40,6 @@ def minimax_decision(state, turn):
                 if m > max:
                     max = m
                     best_move = move
-                    utility += 1
 
     else:
         min = 10000000000000
@@ -52,9 +50,8 @@ def minimax_decision(state, turn):
                 if m < min:
                     min = m
                     best_move = move
-                    utility -= 1
 
-    return best_move, utility
+    return best_move
 
 
 def negamax_decision(state):
@@ -71,18 +68,61 @@ def negamax_decision(state):
             if newval > max:
                 max = newval
                 best_move = move
-                
 
     return best_move, max
 
 
+def negamax_with_tt(state, depth, alpha, beta, colour, tt):
+    hash_table = hash(state)
+    
+    # Check if the current state is already in the transposition table
+    if hash_table in tt:
+        tt_value = tt[hash_table]
+        if tt_value[0] is not None and tt_value[1] is not None:
+            return tt_value[0], tt_value[1]
+    
+    # Check if the maximum search depth has been reached or the game is over
+    if depth == 0 or state == 1:
+        return -1, 1
+    
+    best_utility = None
+    best_move = None
+    
+    for move in range(1, 4):
+        if state - move > 0:
+            # Make a move and recursively search the resulting state
+            m = negamax_with_tt(state - move, depth - 1, -beta, -alpha, -colour, tt)
+            newval = -m[0]
+            
+            # Update the best move and utility found so far
+            if best_utility is None or newval > best_utility:
+                best_utility = newval
+                best_move = move
+            
+            # Update alpha and check if a beta cutoff occurred
+            alpha = max(alpha, newval)
+            if alpha >= beta:
+                break
+    
+    # Update the transposition table with the best utility and move
+    tt[hash_table] = (best_utility, best_move)
+    
+    return best_utility, best_move
+
+
 def play_nim(state):
+    # Initialize the transposition table
+    tt = {}
     turn = 0
 
     while state != 1:
-        move, value = negamax_decision(state)
+        # Call the modified Negamax function with the transposition table
+        value, move = negamax_with_tt(state, 10, -float("inf"), float("inf"), (-1) ** turn, tt)
         print(str(state) + ": " + ("MAX" if not turn else "MIN") + " takes " + str(move) + ' with utility ' + str(value))
 
+
+
+        #input()
         state -= move
         turn = 1 - turn
 
